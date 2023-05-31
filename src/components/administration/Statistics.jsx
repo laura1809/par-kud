1faaf5cd89accec83bc2b3c299d70b99fb4fd07e
laura1809 <ltrr2001@gmail.com>
2023-05-31 09:30:22 -0500

    add map

--- src/components/administration/Statistics.jsx
+++ src/components/administration/Statistics.jsx
@@ -2,11 +2,13 @@ const Statistics = () => {
   return (
     <>
       <div>Estadísticas</div>
+      
       <iframe
         src="https://lookerstudio.google.com/embed/reporting/0cb6db49-3662-485d-b399-28b65b47c805/page/p04QD"
-        frameborder="0"
+        className="w-full h-screen mx-auto scale-75"
         allowfullscreen
       ></iframe>
+      
     </>
   );
 };
